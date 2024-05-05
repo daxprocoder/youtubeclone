193e84011241b9b9f08cfed1b8820c33932cafac
Dax <daxprocoder@gmail.com>
2024-05-05 18:22:48 +0530

    access key completed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -47,9 +47,6 @@ const userSchema = new Schema({
         type:String
     }
     
-
-
-
 },{
     timestamps:true
 })  
@@ -64,6 +61,31 @@ userSchema.pre("save",async function(next){
 userSchema.methods.isPasswordCorrect = async function
 (password){
     return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+},
+        process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY  
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESHj_TOKEN_EXPIRY
+    }
+    )
 }
 
 export const User = mongoose.model("User",userSchema)

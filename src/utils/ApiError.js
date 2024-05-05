7937e06605dfc8348d1c0a4989acc2ba45a16386
Dax <daxprocoder@gmail.com>
2024-05-05 11:13:11 +0530

    Create ApiError.js

---
+++ src/utils/ApiError.js
@@ -0,0 +1,18 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message = "something went wrong",
+        error = [],
+        statck = " "
+    ){
+        super(message)
+        this.statusCode =statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = this.errors
+
+    }
+}
+
+export {ApiError}

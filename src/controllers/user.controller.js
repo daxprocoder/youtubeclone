de8ec06780021bed8d2ef2adf7305951db364d0c
Dax <daxprocoder@gmail.com>
2024-05-09 00:15:17 +0530

    cookie config

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,24 @@ import { uploadOnCloudnary } from "../utils/cloudnairy.js"
 import { APiResponse } from "../utils/ApiResponse.js";
 
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave:false})
+
+        return {accessToken,refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating refresh and access token")
+    }
+}
+
+
+
 const registerUser = asyncHandler(async (req,res) => {
         const {fullName , email , username ,password } = req.body
         console.log("email:",email);
@@ -100,9 +118,28 @@ const loginUser = asyncHandler(async(req,res)=>{
     }
 
 
-    await User
+    const isPasswordValid= await user.idPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "user credentaials")
+    }
 
 
+    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
+    
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const option = {
+        httpOnly:true,
+        secure:true
+    }
+
+    return res.status(200).cookie("accessToken",accessToken,option).cookie("refreshToken",refreshToken,options).json(
+        200,{
+            user:loggedInUser,accessToken,refreshToken
+        },
+        "User logged un Succesfully!!"
+    )
 
 })
 
